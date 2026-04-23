00892a5df82d684f56c5323262ac1d401e0ff3f1
glen ordóñez <149749777+glen-ob@users.noreply.github.com>
2026-04-23 15:15:26 -0600

    inicios de Trivia2 + fixes
    eliminé unos files q no ocupabamos para alivianar el tamaño

--- socc3r-1.0.0/assets/js/trivia2.js
+++ socc3r-1.0.0/assets/js/trivia2.js
@@ -1,6 +1,6 @@
 
 //this would be the object shape for storing the questions  
- //you can change the questions to your own taste or even add more questions..
+//you can change the questions to your own taste or even add more questions..
 const questions = [
     { //1
         question: "How many days makes a week ?",
@@ -231,12 +231,11 @@ const questions = [
 ]
 
 
-let shuffledQuestions = [] //empty array to hold shuffled selected questions out of all available questions
+let shuffledQuestions = [] //arreglo vacio para mezclar las 10 preguntas
 
-function handleQuestions() { 
-    //function to shuffle and push 10 questions to shuffledQuestions array
-//app would be dealing with 10questions per session
-    while (shuffledQuestions.length <= 9) {
+function handleQuestions() {
+    //la función para mezclarlas
+    while (shuffledQuestions.length <= 9) { //permite que tenga 10
         const random = questions[Math.floor(Math.random() * questions.length)]
         if (!shuffledQuestions.includes(random)) {
             shuffledQuestions.push(random)
@@ -245,14 +244,13 @@ function handleQuestions() {
 }
 
 
-let questionNumber = 1 //holds the current question number
-let playerScore = 0  //holds the player score
-let wrongAttempt = 0 //amount of wrong answers picked by player
-let indexNumber = 0 //will be used in displaying next question
+let questionNumber = 1 //para ver en q pregunta va
+let playerScore = 0  //para la calif
+let wrongAttempt = 0 //cantidad d respuestas incorrectas
+let indexNumber = 0 //pa enseñar la siguiente pregunta
 
-// function for displaying next question in the array to dom
-//also handles displaying players and quiz information to dom
 function NextQuestion(index) {
+    // función pa mostrar la pregunta q sigue junto con la info del "jugador"
     handleQuestions()
     const currentQuestion = shuffledQuestions[index]
     document.getElementById("question-number").innerHTML = questionNumber
@@ -262,35 +260,31 @@ function NextQuestion(index) {
     document.getElementById("option-two-label").innerHTML = currentQuestion.optionB;
     document.getElementById("option-three-label").innerHTML = currentQuestion.optionC;
     document.getElementById("option-four-label").innerHTML = currentQuestion.optionD;
-
 }
 
 
 function checkForAnswer() {
-    const currentQuestion = shuffledQuestions[indexNumber] //gets current Question 
-    const currentQuestionAnswer = currentQuestion.correctOption //gets current Question's answer
-    const options = document.getElementsByName("option"); //gets all elements in dom with name of 'option' (in this the radio inputs)
+    const currentQuestion = shuffledQuestions[indexNumber] //agarra la pregunta actual
+    const currentQuestionAnswer = currentQuestion.correctOption //agarra la respuesta correcta
+    const options = document.getElementsByName("option"); //saca todas las opciones
     let correctOption = null
 
     options.forEach((option) => {
         if (option.value === currentQuestionAnswer) {
-            //get's correct's radio input with correct answer
             correctOption = option.labels[0].id
         }
     })
 
-    //checking to make sure a radio input has been checked or an option being chosen
     if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
         document.getElementById('option-modal').style.display = "flex"
     }
 
-    //checking if checked radio button is same as answer
+    //pa checar si la escogida es correctaaaa o ne
     options.forEach((option) => {
         if (option.checked === true && option.value === currentQuestionAnswer) {
             document.getElementById(correctOption).style.backgroundColor = "green"
-            playerScore++ //adding to player's score
-            indexNumber++ //adding 1 to index so has to display next question..
-            //set to delay question number till when next question loads
+            playerScore++
+            indexNumber++
             setTimeout(() => {
                 questionNumber++
             }, 1000)
@@ -300,9 +294,8 @@ function checkForAnswer() {
             const wrongLabelId = option.labels[0].id
             document.getElementById(wrongLabelId).style.backgroundColor = "red"
             document.getElementById(correctOption).style.backgroundColor = "green"
-            wrongAttempt++ //adds 1 to wrong attempts 
+            wrongAttempt++
             indexNumber++
-            //set to delay question number till when next question loads
             setTimeout(() => {
                 questionNumber++
             }, 1000)
@@ -312,24 +305,22 @@ function checkForAnswer() {
 
 
 
-//called when the next button is called
 function handleNextQuestion() {
-    checkForAnswer() //check if player picked right or wrong option
+    checkForAnswer()
     unCheckRadioButtons()
-    //delays next question displaying for a second just for some effects so questions don't rush in on player
     setTimeout(() => {
         if (indexNumber <= 9) {
-//displays next question as long as index number isn't greater than 9, remember index number starts from 0, so index 9 is question 10
+            //revisa q no haya sido la 10ma pregunta ya
             NextQuestion(indexNumber)
         }
         else {
-            handleEndGame()//ends game if index number greater than 9 meaning we're already at the 10th question
+            handleEndGame()
         }
         resetOptionBackground()
     }, 1000);
 }
 
-//sets options background back to null after display the right/wrong colors
+//resetea tonos de los botones
 function resetOptionBackground() {
     const options = document.getElementsByName("option");
     options.forEach((option) => {
@@ -337,7 +328,7 @@ function resetOptionBackground() {
     })
 }
 
-// unchecking all radio buttons for next question(can be done with map or foreach loop also)
+//quita lo seleccionado
 function unCheckRadioButtons() {
     const options = document.getElementsByName("option");
     for (let i = 0; i < options.length; i++) {
@@ -365,7 +356,6 @@ function handleEndGame() {
     }
     const playerGrade = (playerScore / 10) * 100
 
-    //data to display to score board
     document.getElementById('remarks').innerHTML = remark
     document.getElementById('remarks').style.color = remarkColor
     document.getElementById('grade-percentage').innerHTML = playerGrade
@@ -375,7 +365,7 @@ function handleEndGame() {
 
 }
 
-//closes score modal, resets game and reshuffles questions
+//REINICIA el loop !!
 function closeScoreModal() {
     questionNumber = 1
     playerScore = 0
@@ -386,7 +376,6 @@ function closeScoreModal() {
     document.getElementById('score-modal').style.display = "none"
 }
 
-//function to close warning modal
 function closeOptionModal() {
     document.getElementById('option-modal').style.display = "none"
 }
