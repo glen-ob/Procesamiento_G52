d214fce1beb2ca952a0be09afa84f200aa544084
glen ordóñez <practicante1@tacticsunlimited.com>
2026-04-30 15:02:19 -0600

    traductor p4

--- socc3r-1.0.0/assets/js/traductor.js
+++ socc3r-1.0.0/assets/js/traductor.js
@@ -1,28 +1,37 @@
 const translations = {
     mx: {
-        // inicio: "inicio",
-        // galeria: "galeria",
-        // trivia: "trivia",
-        // scan: "escaneo",
-        // minijuego: "minijuego",
-        // btnTourIndex: "ayuda",
+        //NAVBAR
+        inicio: "inicio",
+        galeria: "galeria",
+        trivia: "trivia",
+        scan: "escaneo",
+        minijuego: "minijuego",
+        btnTourIndex: "ayuda",
+        //Hero Section Index
         bienv: "Bienvenido a ",
         scr: "Socc3r",
         desc: "Donde tienes tu colección de cartas favoritas!",
+        //About Section
         h3: "Socc3r Trading Cards",
-        h3p: "Balones de Mundiales, Estadios y Equipos"
-
+        h3p: "Balones de Mundiales, Estadios y Equipos",
+        calidad: "Alta calidad.",
+        favs: "Con tus equipos favoritos.",
+        info: "Los equipos, balones y estadios del Mundial 2026 ya se encuentran disponibles.",
+        extra: "  Una de las colecciones más prestigiosas de cromos de futbol y trading cards de la más alta calidad con muchas series distintas y una gran cantidad de cromos de futbol con firmas y con diferentes versiones numeradas. Ha conquistado a los grandes coleccionistas de todo el mundo por su complejidad."
     },
     en: {
-        // inicio: "index",
-        // galeria: "gallery",
-        // trivia: "trivia",
-        // scan: "scanning",
-        // minijuego: "minigame",
-        // btnTourIndex: "help",
+        //NAVBAR
+        inicio: "index",
+        galeria: "gallery",
+        trivia: "trivia",
+        scan: "scanning",
+        minijuego: "minigame",
+        btnTourIndex: "help",
+        //Hero Section Index
         bienv: "Welcome to ",
         scr: "Socc3r",
         desc: "Where you keep your favorite collection of cards!",
+        //About Section
         h3: "Socc3r Trading Cards",
         h3p: "World Cups, stadiums and countries"
     }
